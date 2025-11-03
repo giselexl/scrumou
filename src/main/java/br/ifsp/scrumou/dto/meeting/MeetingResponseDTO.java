121576952d74e4f17a4e3c7fd5f77658b896fd30
natalia <nataliadasilva.t@gmail.com>
2025-11-03 20:15:35 -0300

    refactor: add Lombok annotations to MeetingResponseDTO for cleaner code

--- src/main/java/br/ifsp/scrumou/dto/meeting/MeetingResponseDTO.java
+++ src/main/java/br/ifsp/scrumou/dto/meeting/MeetingResponseDTO.java
@@ -3,6 +3,11 @@ package br.ifsp.scrumou.dto.meeting;
 import java.time.LocalDate;
 
 import br.ifsp.scrumou.model.Sprint;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MeetingResponseDTO {
     private Long id;
     private String title;
